be75227d90841046b18478786fd4aae4b1609026
AshwinKatkar <ashwin.katkar@craveinfotech.com>
2022-02-16 12:14:02 +0000

    typo issue fixed

---
+++ webapp/ext/controller/ListReportExt.controller.js
@@ -0,0 +1,106 @@
+sap.ui.define(
+    [
+      "sap/ui/model/Filter",
+      "sap/ui/comp/smartfilterbar/SmartFilterBar",
+      "sap/m/ComboBox",
+    ],
+    function (Filter, SmartFilterBar, ComboBox) {
+      "use strict";
+      var oExtPeriodModel;
+      return {
+        onInit: function (oEvent) {
+          oExtPeriodModel = new sap.ui.model.json.JSONModel();
+          this.getOwnerComponent().setModel(oExtPeriodModel, "oExtPeriodModel");
+          this.getView().byId("escalationreportscc::sap.suite.ui.generic.template.ListReport.view.ListReport::Report_Submit_to_Under_Review--listReport-1").setSmartVariant();
+          this.getView().byId("escalationreportscc::sap.suite.ui.generic.template.ListReport.view.ListReport::Report_Submit_to_Under_Review--listReport-2").setSmartVariant();
+          this.getView().byId("escalationreportscc::sap.suite.ui.generic.template.ListReport.view.ListReport::Report_Submit_to_Under_Review--listReportFilter").setPersistencyKey(true);  
+      },
+  /*      getCustomAppStateDataExtension: function (oCustomData) {
+          //the content of the custom field will be stored in the app state, so that it can be restored later, for example after a back navigation.
+          //The developer has to ensure that the content of the field is stored in the object that is passed to this method.
+          if (oCustomData) {
+            var oCustomField1 = this.oView.byId("ExtYear");
+            if (oCustomField1) {
+              oCustomData.ExtYearName = oCustomField1.getSelectedKey();
+            }
+          }
+        },
+        restoreCustomAppStateDataExtension: function (oCustomData) {
+          //in order to restore the content of the custom field in the filter bar, for example after a back navigation,
+          //an object with the content is handed over to this method. Now the developer has to ensure that the content of the custom filter is set to the control
+          if (oCustomData) {
+            if (oCustomData.ExtYearName) {
+              var oComboBox = this.oView.byId("ExtYear");
+              oComboBox.setSelectedKey(oCustomData.ExtYearName);
+            }
+          }
+        },
+        onBeforeRebindTableExtension: function (oEvent) {
+          var oBindingParams = oEvent.getParameter("bindingParams");
+          oBindingParams.parameters = oBindingParams.parameters || {};
+          var vExtYear = this.byId("ExtYear").getSelectedKey();
+          if (
+            vExtYear === null ||
+            vExtYear === "" ||
+            vExtYear === undefined ||
+            vExtYear === ""
+          ) {
+          } else {
+            oBindingParams.filters.push(new Filter("CRFYear", "EQ", vExtYear));
+            oBindingParams.filters.push(
+              new Filter("PSInvoiceFYear", "EQ", vExtYear)
+            );
+          }
+          var vExtPeriod = this.byId("ExtPeriod").getSelectedKey();
+          if (
+            vExtPeriod === null ||
+            vExtPeriod === "" ||
+            vExtPeriod === undefined ||
+            vExtPeriod === ""
+          ) {
+          } else {
+            oBindingParams.filters.push(
+              new Filter("PSInvoicePeriod", "EQ", vExtPeriod)
+            );
+            oBindingParams.filters.push(new Filter("CRPeriod", "EQ", vExtPeriod));
+          }
+        },
+        selectionChangeYear: function (oEvent) {
+          var extPeriod = sap.ui.getCore().byId("ExtPeriod");
+          var oModel = this.getOwnerComponent().getModel();
+          var oFilterR = new sap.ui.model.Filter({
+            filters: [
+              new sap.ui.model.Filter(
+                "FYCODE",
+                "EQ",
+                oEvent.getParameter("selectedItem").getKey()
+              ),
+            ],
+          });
+          var that = this;
+          oModel.read("/GetFiscalPeriod", {
+            filters: [oFilterR],
+            success: function (oResponse) {
+              if (oResponse.results.length > 0) {
+                oResponse.results.splice(0, 0, {
+                  FYCODE: "",
+                  PERIODID: "",
+                  Name: "All",
+                });
+                that.oView.byId("ExtPeriod").setValue("All");
+              } else {
+                that.oView.byId("ExtPeriod").setValue();
+              }
+              that
+                .getOwnerComponent()
+                .getModel("oExtPeriodModel")
+                .setProperty("/", oResponse.results);
+            },
+            error: function (err) {},
+          });
+        }, 
+        */
+      };
+    }
+  );
+  
